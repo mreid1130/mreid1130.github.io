85794e664104a34bb9344390f5f851ec57b2fa09
Mark Reid <mark.francis.reid@gmail.com>
2015-02-26 15:08:26 -0800

    adds angular blog controller

--- public/angular/controllers/blogController.js
+++ public/angular/controllers/blogController.js
@@ -1,5 +1,14 @@
 angular.module('blogController', [])
 
 .controller('BlogCtrl', ['$scope', function($scope){
-	
+	$scope.blogInfo = [{path: './blog/post-dbc-reflection.html',
+						 title: 'Post DBC Reflection',
+						 date: 'Feb. 26, 2015'
+						}]
+
+	$scope.selectBlogPost = function(blogpath){
+		// $scope.blogPost = blogpath
+		console.log('working: ', blogpath)
+	}
+
 }])
